86a7d297a498f59a6629ff62930958e306303b81
babeyJUJU <babeyjuju@gmail.com>
2026-05-26 15:29:56 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,7 +14,7 @@ DATABASE_URL: process.env.DATABASE_URL || '',
 
 botName: process.env.BOT_NAME || 'SUBZERO-MD',
 botOwner: process.env.BOT_OWNER || 'Darrell M',
-ownerNumber: process.env.OWNER_NUMBER || '263719647303',
+ownerNumber: process.env.OWNER_NUMBER || '254753012677',
 author: process.env.AUTHOR || 'Mr Frank OFC',
 packname: process.env.PACKNAME || 'SUBZERO-MD',
 description: process.env.DESCRIPTION || 'Multi-device WhatsApp bot',
